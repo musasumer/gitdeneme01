cf0f1baedebc6490a841309b5b946a8f1e30b4c1
musasumer <musasumer2020@gmail.com>
2020-06-22 10:21:54 +0300

    ilk denememi localden gonderiyorum

---
+++ src/gitdeneme_01/GitDeneme01.java
@@ -0,0 +1,14 @@
+package gitdeneme_01;
+
+public class GitDeneme01 {
+
+	public static void main(String[] args) {
+		
+		int a = 11;
+		int b = 22;
+		int sum = a+b;
+		System.out.println(sum);
+
+	}
+
+}
